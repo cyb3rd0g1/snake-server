2e445c861cdb5570cf414a03bb1fb1e6a3457394
dustinduse <dustinduse@dusecraft.com>
2018-12-30 10:26:19 -0600

    Added respawn mechanics and QOL fixes

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ function validatePlayerPosition (position) {
 	return true;
 }
 
+function setPlayerAlive(playerID) {
+	players[playerID].alive = true;
+}
+
 function randomizePlayerPosition(playerID) {
 
 	//choose random spawn for player
@@ -57,17 +61,22 @@ function randomizePlayerPosition(playerID) {
 	players[playerID].tail = [ ...position ];
 	players[playerID].headDirection = direction;
 	players[playerID].tailDirections = [direction];
+	players[playerID].alive = false;
 	
-	//TODO check that player is at least three spaces away from a wall or another player
-
+	//Delay the snake movement for 2 seconds
+	setTimeout(() => {
+		setPlayerAlive(playerID);
+	 }, 2000);
+	
+	//TODO check that player is at least three moves away from a wall or another player
 	/* Search this area for collisions
-		   .
-		  ...
-		 .....
-		...X...
-		 .....
-	      ...
-		   .
+		   .          Y + 3
+		  ...         Y + 2 & X + - 1
+		 .....        Y + 1 & X + - 2
+		...X...       X + - 3
+		 .....        Y - 1 & X + - 2
+	      ...         Y - 2 & X + - 1
+		   .          Y - 3
 	*/
 }
 
@@ -97,54 +106,73 @@ function killSnake(playerID) {
 			tail = [tail[0], ++tail[1]];
 		}	
 	}
+	console.log("Player " + playerID + " has died.")
+}
+
+function spawnDeadPlayer(player) {
+	//Check that player is actually dead
+	if(players[player].alive === false) {
+		//Spawn the player
+		randomizePlayerPosition(player);
+		//Wait 2 seconds before setting player alive so the snake is stationary.
+		setTimeout(() => {
+			setPlayerAlive(player);
+		 }, 2000);
+	}
 }
 
 function move() {
 	if (Object.keys(players).length > 0) {
 		Object.keys(players).forEach(player => {
-			const headDirection = players[player].headDirection;
-			let head = [ ...players[player].head ];
-			let tail = [ ...players[player].tail ];
-			let tailDirections = [ ...players[player].tailDirections ];
+			//Verify player is alive before running movement calculations
+			if(players[player].alive !== false) {
+				const headDirection = players[player].headDirection;
+				let head = [ ...players[player].head ];
+				let tail = [ ...players[player].tail ];
+				let tailDirections = [ ...players[player].tailDirections ];
 
-			tailDirections.push(headDirection);
+				tailDirections.push(headDirection);
 
-			if(headDirection === 0 && (head[0] + 1 < height) && ((gameState[head[0] + 1][head[1]] === ".") || (gameState[head[0] + 1][head[1]] === "@"))){
-				head = [++head[0], head[1]];
-			}else if (headDirection === 1 && (head[0] - 1 >= 0) && ((gameState[head[0] - 1][head[1]] === ".") || (gameState[head[0] - 1][head[1]] === "@"))){
-				head = [--head[0], head[1]];
-			}else if (headDirection === 2 && (head[1] - 1 >= 0) && ((gameState[head[0]][head[1] - 1] === ".") || (gameState[head[0]][head[1] - 1] === "@"))){
-				head = [head[0], --head[1]];
-			}else if (headDirection === 3 && (head[1] + 1 < width) && ((gameState[head[0]][head[1] + 1 ] === ".") || (gameState[head[0]][head[1] + 1 ] === "@"))){
-				head = [head[0], ++head[1]];
-			}else{
-				killSnake(player);
-				randomizePlayerPosition(player);
-				return;
-			}
-
-			//conditional logic to determine if snake grows
-			if(gameState[head[0]][head[1]] !== "@"){
-				let tailDirection = tailDirections.shift();
-				gameState[tail[0]][tail[1]] = ".";
-				if(tailDirection === 0 && (tail[0] + 1 < height)){
-					tail = [++tail[0], tail[1]];
-				}else if (tailDirection === 1 && (tail[0] - 1 >= 0)){
-					tail = [--tail[0], tail[1]];
-				}else if (tailDirection === 2 && (tail[1] - 1 >= 0)){
-					tail = [tail[0], --tail[1]];
-				}else if (tailDirection === 3 && (tail[1] + 1 < width)){
-					tail = [tail[0], ++tail[1]];
+				if(headDirection === 0 && (head[0] + 1 < height) && ((gameState[head[0] + 1][head[1]] === ".") || (gameState[head[0] + 1][head[1]] === "@"))){
+					head = [++head[0], head[1]];
+				}else if (headDirection === 1 && (head[0] - 1 >= 0) && ((gameState[head[0] - 1][head[1]] === ".") || (gameState[head[0] - 1][head[1]] === "@"))){
+					head = [--head[0], head[1]];
+				}else if (headDirection === 2 && (head[1] - 1 >= 0) && ((gameState[head[0]][head[1] - 1] === ".") || (gameState[head[0]][head[1] - 1] === "@"))){
+					head = [head[0], --head[1]];
+				}else if (headDirection === 3 && (head[1] + 1 < width) && ((gameState[head[0]][head[1] + 1 ] === ".") || (gameState[head[0]][head[1] + 1 ] === "@"))){
+					head = [head[0], ++head[1]];
+				}else{
+					killSnake(player);
+					players[player].alive = false;
+					setTimeout(() => {
+						spawnDeadPlayer(player);
+					 }, 3000);
+					return;
 				}
-			}else{
-				placeFood();
-			}
 
-			gameState[head[0]][head[1]] = player;      
-			players[player].head = [ ...head ];
-			players[player].tail = [ ...tail ];
-			players[player].headDirection = headDirection;
-			players[player].tailDirections = [ ...tailDirections ];
+				//conditional logic to determine if snake grows
+				if(gameState[head[0]][head[1]] !== "@"){
+					let tailDirection = tailDirections.shift();
+					gameState[tail[0]][tail[1]] = ".";
+					if(tailDirection === 0 && (tail[0] + 1 < height)){
+						tail = [++tail[0], tail[1]];
+					}else if (tailDirection === 1 && (tail[0] - 1 >= 0)){
+						tail = [--tail[0], tail[1]];
+					}else if (tailDirection === 2 && (tail[1] - 1 >= 0)){
+						tail = [tail[0], --tail[1]];
+					}else if (tailDirection === 3 && (tail[1] + 1 < width)){
+						tail = [tail[0], ++tail[1]];
+					}
+				}else{
+					placeFood();
+				}
+
+				gameState[head[0]][head[1]] = player;      
+				players[player].head = [ ...head ];
+				players[player].tail = [ ...tail ];
+				players[player].headDirection = headDirection;
+				players[player].tailDirections = [ ...tailDirections ];
+			}
 		});
 		
 		io.emit('gamestate', gameState);
@@ -159,46 +187,46 @@ function playerResetEvent() {
 
 //Called by Move listener
 function playerMoveEvent(data, newPlayer) {
-	console.log(data);
+	//console.log(newPlayer + " - " + data);
+	var headDirection = players[newPlayer].headDirection;
+	var tailDirections = [ ...players[newPlayer].tailDirections ];
 
-		var headDirection = players[newPlayer].headDirection;
-      		var tailDirections = [ ...players[newPlayer].tailDirections ];
+	if(data === "down" && headDirection !== 1){
+		headDirection = 0;
+	}else if(data === "up" && headDirection !== 0){
+		headDirection = 1;
+	}else if(data === "left" && headDirection !== 3){
+		headDirection = 2;
+	}else if(data === "right" && headDirection !== 2){
+		headDirection = 3;
+	}	
 
-      		if(data === "down" && headDirection !== 1){
-        		headDirection = 0;
-      		}else if(data === "up" && headDirection !== 0){
-        		headDirection = 1;
-      		}else if(data === "left" && headDirection !== 3){
-        		headDirection = 2;
-      		}else if(data === "right" && headDirection !== 2){
-        		headDirection = 3;
-      		}	
+	if(headDirection !== players[newPlayer].headDirection){
+		tailDirections.pop();
+		tailDirections.push(headDirection);
+	}
 
-      		if(headDirection !== players[newPlayer].headDirection){
-        		tailDirections.pop();
-        		tailDirections.push(headDirection);
-      		}
-
-        	players[newPlayer].headDirection = headDirection;
-        	players[newPlayer].tailDirections = [ ...tailDirections ];
+	players[newPlayer].headDirection = headDirection;
+	players[newPlayer].tailDirections = [ ...tailDirections ];
 }
 
 function getRandomColor() {
-	var letters = '0123456789ABCDEF';
+	//Generates random color code that is between 333333 and CCCCCC.
+	var letters = '3456789ABC';
 	var color = '#';
 	for (var i = 0; i < 6; i++) {
-	  color += letters[Math.floor(Math.random() * 16)];
+	  color += letters[Math.floor(Math.random() * 10)];
 	}
 	return color;
   }
 
 function randomizePlayerColor(playerID) {
+	//Ran on new player join, Generates, sets and then sends the new color data.
 	colors[playerID] = getRandomColor();
 	io.emit('colors', colors);
 }
 
 function setPlayerStart(socket) {
-
 	//Verify we dont have any active listeners on a socket.
 	socket.removeAllListeners();
 
@@ -211,6 +239,8 @@ function setPlayerStart(socket) {
 
 	randomizePlayerColor(newPlayer); 
 
+	console.log("Player " + newPlayer + " has joined with the color " + colors[newPlayer])
+
 	//register movement listener
 	players[newPlayer].socket.on('move', (data) => playerMoveEvent(data, newPlayer));
 
@@ -221,10 +251,11 @@ function setPlayerStart(socket) {
 	players[newPlayer].socket.on('disconnect', () => {
 		killSnake(newPlayer);
 		players[newPlayer].socket.disconnect();
+		console.log("Player " + newPlayer + " has left the game.")
 		delete players[newPlayer];
 	});
 
-	//Return gamestate to client
+	//Return gamestate to all clients
 	io.emit('gamestate', gameState);
 }
 
